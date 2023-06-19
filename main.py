c38c97e1e03bc31d039f3cd6f826c04e11df1bd0
Manlosmade <encryptforwarding@duck.com>
2023-06-19 20:40:50 +0100

    Learnt about rectanges, screen.blit() and displays

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
 from sys import exit
 pygame.init()
-screen = pygame.display.set_mode((1200,600))
+screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("PYGAME RUNNER")
 clock = pygame.time.Clock()
+font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('Red')
+skySurface = pygame.image.load('graphics/Sky.png').convert_alpha()
+groundSurface = pygame.image.load('graphics/Ground.png').convert_alpha()
+scoreSurface = font.render('Score: ', False, 'Black').convert_alpha()
+
+
+snailSurface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+playerSurface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+
+playerCollision = playerSurface.get_rect(midbottom = (80,300))
+snailCollision = snailSurface.get_rect(midbottom = (700, 300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,7 +25,14 @@ while True:
             exit()
 
     
-    screen.blit(test_surface,(200,100))
-
+    screen.blit(skySurface,(0,0))
+    screen.blit(groundSurface,(0,300))
+    screen.blit(scoreSurface,(300,50))
+    screen.blit(snailSurface, snailCollision)
+    screen.blit(playerSurface, playerCollision)
+    
+    snailCollision.x -= 1
+    if snailCollision.right <= 0:
+        snailCollision.left = 800
     pygame.display.update()
     clock.tick(120)
