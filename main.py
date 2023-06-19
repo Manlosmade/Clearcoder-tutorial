1c7f55ad06778a19a0d6784a7d9b50003f8e1eef
Manlosmade <encryptforwarding@duck.com>
2023-06-19 20:02:00 +0100

    Cotinuing

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ screen = pygame.display.set_mode((1200,600))
 pygame.display.set_caption("PYGAME RUNNER")
 clock = pygame.time.Clock()
 
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all out elements and update everything
+
+    
+    screen.blit(test_surface,(200,100))
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(120)
